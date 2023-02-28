bd4a649ab72daf94bedef6ebfd6f98abf8ee6a42
ThaariqAqil <114320553+ThaariqAqil@users.noreply.github.com>
2023-02-28 21:18:14 +0700

    Add files via upload

---
+++ prakweb2/week1/test.php
@@ -0,0 +1,16 @@
+<?php
+// ======================================
+// definisikan variable
+$nama = "Assalaamu'alaikum Thaariq Aqil";
+$umur = 20;
+$berat = 57;
+
+echo 'Nama : '.$nama;
+echo '<br> Umur : '.$umur.'Tahun';
+echo '<br> Berat : '.$berat.' kg ';
+
+echo "<hr>";
+
+echo "<br/Hello> $nama ApaKabar?";
+echo "<br/Hello> Alhamdulillah Kabar saya baik";
+?>
