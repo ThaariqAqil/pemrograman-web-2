6f09bef609ab32151335247197b2f4fec4246299
ThaariqAqil <114320553+ThaariqAqil@users.noreply.github.com>
2023-06-07 18:21:57 +0700

    Praktikum 3

---
+++ week3/week3/template/index.php
@@ -0,0 +1,17 @@
+<!-- Panggil file atas.php -->
+<?php
+require_once('atas.php');
+?>
+<!-- /Tutup file content.php -->
+
+<!-- Panggil file content.php -->
+<?php
+require_once('content.php')
+?>
+<!-- /Tutup File content.php -->
+
+<!-- Panggil file bawah.php -->
+<?php
+require_once('bawah.php');
+?>
+<!-- /Tutup file bawah.php -->
